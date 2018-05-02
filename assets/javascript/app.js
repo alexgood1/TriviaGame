82410adcb645918695b5030bd4ed89aa60fac6d1
AMG <alexgood1@gmail.com>
2018-05-01 17:08:58 -0700

    initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+// $("document").ready(function(){
+
+// start screen - html with a jquery click event
+
+// variables: number of seconds(setInterval), questions array, amount of answers correct, amt incorrect, amt unanswered, 
+
+// attr inside buttons to allow to only select one 
+// .attr(); - takes in attr and value (2 paramaters)
+// .attr("parameter","value");
+
+// });
+
+// from 
+
+// ids - append  
+
+// from divs - append data inside those divs 
+
+// all questions inside forms, which are inside divs 
+// event.preventDefault - js built in method 
+
+console.log("Hello!");
