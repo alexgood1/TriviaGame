a7b11ef204896aa7bc668030424678cbb97a2563
AMG <alexgood1@gmail.com>
2018-05-04 20:11:57 -0700

    final edits

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -41,7 +41,7 @@ var questionAnswers = [
         correctAnswer: "folds on his parietal lobe"
       }, {
         question:  "A ____________ helped make Einstein world famous.",
-        answers: ["Presidental Election", "Lunar Eclipse", "Woman", "Solar Eclipe"]
+        answers: ["Presidental Election", "Lunar Eclipse", "Woman", "Solar Eclipe"],
         correctAnswer: "solar eclipse"
     }
 ];
@@ -53,7 +53,7 @@ $('#start').on('click', function(){
   });
   // End my game when my Done btn clicked
   $('#done').on('click', function(){
-    game.countDown();
+    game.done();
   });
 
   // variables: number of seconds(setInterval), questions array, amount of answers correct, amt incorrect, amt unanswered
@@ -71,15 +71,17 @@ $('#start').on('click', function(){
   var game = {
     correct: 0,
     incorrect: 0,
-    counter: 120,
+    counter: 40,
     // Setting my counter
     countDown: function(){
       game.counter--;
       $('#counter').html(game.counter)
       if(game.counter <= 0){
+        clearInterval(timer);
         game.done();
       }
       if($('#done').on('click', function(){
+       clearInterval(timer);
         game.done();
       }));
     //   if(game.counter <= 20){
@@ -89,12 +91,12 @@ $('#start').on('click', function(){
     // start the game with a timer and add all the questions and answers
     start: function(){
       timer = setInterval(game.countDown, 1000)
-      $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>');
+      $('#qContainer').prepend('<h2>Time Remaining: <span id="counter">120</span> Seconds</h2>');
       $('#start, .game-info').remove();
-      for(var i=0; i<questions.length; i++){
-        $('#subwrapper').append('<h3>' + questions[i].question + '</h3>')
-        for(var j=0; j < questions[i].answers.length; j++){
-          $('#subwrapper').append("<input class='check-with-label' type='radio' name='question-"+i+"' value='"+questions[i].answers[j]+"'>  <label class='label-for-check'>"+ questions[i].answers[j]+"  </label></br>");
+      for(var i=0; i<questionAnswers.length; i++){
+        $('#qContainer').append('<h3>' + questionAnswers[i].question + '</h3>')
+        for(var j=0; j < questionAnswers[i].answers.length; j++){
+          $('#qContainer').append("<input class='check-with-label' type='radio' name='question-" + i + "' value='"+questionAnswers[i].answers[j]+"'>  <label class='label-for-check'>"+ questionAnswers[i].answers[j]+"  </label></br>");
         }
       }
       $('<button id="done" class="done-btn" type="button" name="button-done">Done</button>').appendTo('#subwrapper');
@@ -102,7 +104,8 @@ $('#start').on('click', function(){
     // checking each input if it is a correct answer or wrong answer and incrementing my game.correct and game.incorrect
     done: function(){
       $.each($('input[name="question-0"]:checked'), function(){
-        if($(this).val() === questions[0].correctAnswer){
+          console.log(questionAnswers[0].correctAnswer);
+        if($(this).val() === questionAnswers[0].correctAnswer){
           game.correct++;
         }
         else {
@@ -110,7 +113,7 @@ $('#start').on('click', function(){
         }
       });
       $.each($('input[name="question-1"]:checked'), function(){
-        if($(this).val() === questions[1].correctAnswer){
+        if($(this).val() === questionAnswers[1].correctAnswer){
           game.correct++;
         }
         else {
@@ -118,7 +121,7 @@ $('#start').on('click', function(){
         }
       });
       $.each($('input[name="question-2"]:checked'), function(){
-        if($(this).val() === questions[2].correctAnswer){
+        if($(this).val() === questionAnswers[2].correctAnswer){
           game.correct++;
         }
         else {
@@ -126,7 +129,7 @@ $('#start').on('click', function(){
         }
       });
       $.each($('input[name="question-3"]:checked'), function(){
-        if($(this).val() === questions[3].correctAnswer){
+        if($(this).val() === questionAnswers[3].correctAnswer){
           game.correct++;
         }
         else {
@@ -134,7 +137,7 @@ $('#start').on('click', function(){
         }
       });
       $.each($('input[name="question-4"]:checked'), function(){
-        if($(this).val() === questions[4].correctAnswer){
+        if($(this).val() === questionAnswers[4].correctAnswer){
           game.correct++;
         }
         else {
@@ -142,7 +145,7 @@ $('#start').on('click', function(){
         }
       });
       $.each($('input[name="question-5"]:checked'), function(){
-        if($(this).val() === questions[5].correctAnswer){
+        if($(this).val() === questionAnswers[5].correctAnswer){
           game.correct++;
         }
         else {
@@ -150,7 +153,7 @@ $('#start').on('click', function(){
         }
       });
       $.each($('input[name="question-6"]:checked'), function(){
-        if($(this).val() === questions[6].correctAnswer){
+        if($(this).val() === questionAnswers[6].correctAnswer){
           game.correct++;
         }
         else {
@@ -158,7 +161,7 @@ $('#start').on('click', function(){
         }
       });
       $.each($('input[name="question-7"]:checked'), function(){
-        if($(this).val() === questions[7].correctAnswer){
+        if($(this).val() === questionAnswers[7].correctAnswer){
           game.correct++;
         }
         else {
@@ -166,7 +169,7 @@ $('#start').on('click', function(){
         }
       });
       $.each($('input[name="question-8"]:checked'), function(){
-        if($(this).val() === questions[8].correctAnswer){
+        if($(this).val() === questionAnswers[8].correctAnswer){
           game.correct++;
         }
         else {
@@ -174,7 +177,7 @@ $('#start').on('click', function(){
         }
       });
       $.each($('input[name="question-9"]:checked'), function(){
-        if($(this).val() === questions[9].correctAnswer){
+        if($(this).val() === questionAnswers[9].correctAnswer){
           game.correct++;
         }
         else {
@@ -182,45 +185,46 @@ $('#start').on('click', function(){
         }
       });
       $.each($('input[name="question-10"]:checked'), function(){
-        if($(this).val() === questions[10].correctAnswer){
+        if($(this).val() === questionAnswers[10].correctAnswer){
           game.correct++;
         }
         else {
           game.incorrect++;
         }
       });
-      // Show the results after checking all answers and play a congrats audio
+      
       this.result();
     },
     // Clearing my timer and remove it from page, adding correct answwers and incorrectanswers and Unanswered questions to the user
+
+//  Cancel on click.
+    // $("#cancel").on("click", function() {
+    // // Clear the button alert's timeout.
+    // clearInterval(countDown);
+    // });
+
+
     result: function(){
       clearInterval(timer);
       $('#subwrapper h2').remove();
       $('#subwrapper').html('<h2>All Done!</h2>').addClass('all-done')
       $('#subwrapper').append("<h3>Correct Answers: "+this.correct+"</h3>");
       $('#subwrapper').append("<h3>Incorrect Answers: "+this.incorrect+"</h3>");
-      // display all the Unanswered questions
-      $('#subwrapper').append("<h3>Unanswered: "+(questions.length-(this.incorrect+this.correct))+"<h3>");
+      // display all Unanswered questions
+      $('#subwrapper').append("<h3>Unanswered: "+(questionAnswers.length-(this.incorrect+this.correct))+"<h3>");
     },
 }  
 
 
-
-
-
-
-
-
-
 var startScreen;
 var gameHTML;
-var counter = 30;
+var counter = 0;
 
 var questionCounter = 0;
 var selecterAnswer;
 var theClock;
-var correctTally = 0;
-var incorrectTally = 0;
+var userPoints= 0;
+var userLosses = 0;
 var unansweredTally = 0;
 
 
@@ -231,11 +235,5 @@ function initialScreen() {
         $("mainArea").html(startScreen);
     }
 
-    initialScreen();
-
-    //Create a function that is triggered by the start button and generates the HTML / html w/ a jquery click event
-
-    // Closes start-button click
-
-
-
+initialScreen();
+})
